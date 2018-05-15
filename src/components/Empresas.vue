d73e0c14d8cd228377ff883d7293060b5348e15f
mcallisaya <mcallisaya@agetic.gob.bo>
2018-05-15 19:24:37 -0400

    agregar empresa

--- src/components/Empresas.vue
+++ src/components/Empresas.vue
@@ -1,14 +1,24 @@
 <template>
   <section>
     <h2>Empresas</h2>
+    <form @submit="agregar">
+      Nombre: <input type="text" v-model="nombre"> <br>
+      Dirección: <input type="text" v-model="direccion"> <br>
+      <button type="button">Cancelar</button>
+      <button type="submit">Agregar</button>
+    </form>
     <table>
       <tr>
         <th>Nombre</th>
         <th>Dirección</th>
+        <th>Acciones</th>
       </tr>
       <tr v-for="item in empresas" :key="item.id">
         <td>{{ item.nombre }}</td>
         <td>{{ item.direccion }}</td>
+        <td>
+          <button type="button" @click="eliminar(item.id)">Eliminar</button>
+        </td>
       </tr>
     </table>
   </section>
@@ -20,16 +30,43 @@ const url = 'http://localhost:4000/api-rest/empresas';
 
 export default {
   created () {
-    axios.get(url)
-    .then((respuesta) => {
-      console.log(respuesta.data)
-      this.empresas = respuesta.data
-      console.log(this.empresas)
-    })
+    this.lista()
   },
   data () {
     return {
-      empresas: []
+      empresas: [],
+      nombre: '',
+      direccion: ''
+    }
+  },
+  methods: {
+    agregar () {
+      console.log(this.nombre, this.direccion)
+      const data = {
+        nombre: this.nombre,
+        direccion: this.direccion
+      }
+      axios.post(url, data)
+      .then(respuesta => {
+        console.log(respuesta)
+        this.lista()
+      })
+    },
+    lista () {
+      axios.get(url)
+      .then((respuesta) => {
+        console.log(respuesta.data)
+        this.empresas = respuesta.data
+        console.log(this.empresas)
+      })
+    },
+    eliminar (id) {
+      if (confirm('¿Eliminar?')) {
+        axios.delete(`${url}/${id}`) // url + '/' + id
+        .then(respuesta => {
+          this.lista()
+        })
+      }
     }
   }
 }
